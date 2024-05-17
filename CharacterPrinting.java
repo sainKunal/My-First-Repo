d8463e7e9bb5705ffe637c51957a985f78d14f2e
sainKunal <Kunalsain0324@gmail.com>
2024-05-17 16:18:12 +0530

    Initial Commit

---
+++ CharacterPrinting.java
@@ -0,0 +1,17 @@
+
+public class CharacterPrinting {
+    public static void main(String args[]) {
+        char ch = 'K';
+        char ch2 = 'U';
+        char ch3 = 'N';
+        char ch4 = 'A';
+        char ch5 = 'L';
+
+        System.out.println("ASCII value of "  + ch + " is " + (int) ch + ".");
+        System.out.println("ASCII value of "  + ch2 + " " + (int) ch2);
+        System.out.println("ASCII value of "  + ch3 + " " +(int) ch3);
+        System.out.println("ASCII value of "  + ch4 + " " + (int) ch4);
+        System.out.println("ASCII value of "  + ch5 + " " +(int) ch5);
+
+    }
+}
---
+++ Loops.java
@@ -0,0 +1,18 @@
+public class Loops {
+    public static void main(String[] args) {
+        
+
+        
+
+    // while ( int i <= n , n++)
+       
+
+
+
+
+
+
+    
+    }
+    
+}
