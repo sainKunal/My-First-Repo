c8537e9ea75206e1da2efd263c989653320d089d
sainKunal <Kunalsain0324@gmail.com>
2024-05-17 16:30:39 +0530

    I did character

--- Loops.java
+++ Loops.java
@@ -1,8 +1,8 @@
 public class Loops {
     public static void main(String[] args) {
-        
+        char a = 'b';
 
-        
+       System.out.println(a); 
 
     // while ( int i <= n , n++)
        
